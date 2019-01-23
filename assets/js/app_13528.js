6345f77ce711a8c4886ad98191065e5c682120bc
Dobby233Liu <papaqi1234567@126.com>
2019-01-23 15:11:08 +0800

    Rude Blusher: Ralsei Not Raisel

--- assets/js/app_13528.js
+++ assets/js/app_13528.js
@@ -1,9 +1,10 @@
 var ralseiFacer = document.getElementById("ralseifacer");
 
 function ralsei_please_switch_your_face(){
-	if(ralseiFacer.src == "./assets/img/raisel_hope.png"){
-		ralseiFacer.src = "./assets/img/raisel_blush.png"; 
+	// rude blusher: ralsei
+	if(ralseiFacer.src == "./assets/img/ralsei_hope.png"){
+		ralseiFacer.src = "./assets/img/ralsei_blush.png"; 
 	} else {
-		ralseiFacer.src = "./assets/img/raisel_hope.png";
+		ralseiFacer.src = "./assets/img/ralsei_hope.png";
 	}
 }
