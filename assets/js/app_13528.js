8cbc5a4a3c7550e0a4be98a4d15e0be60afeb6c9
Dobby233Liu <papaqi1234567@126.com>
2019-01-23 16:21:11 +0800

    yes

--- assets/js/app_13528.js
+++ assets/js/app_13528.js
@@ -1,10 +1,8 @@
-var ralseiFacer = document.getElementById("ralseifacer");
-
 function ralsei_please_switch_your_face(){
 	// rude blusher: ralsei
-	if(ralseiFacer.src == "./assets/img/ralsei_hope.png"){
-		ralseiFacer.src = "./assets/img/ralsei_blush.png"; 
+	if(document.getElementById("ralseifacer").src == "./assets/img/ralsei_hope.png"){
+		document.getElementById("ralseifacer").src = "./assets/img/ralsei_blush.png"; 
 	} else {
-		ralseiFacer.src = "./assets/img/ralsei_hope.png";
+		document.getElementById("ralseifacer").src = "./assets/img/ralsei_hope.png";
 	}
 }
