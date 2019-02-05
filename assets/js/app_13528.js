4e6f3c32655475c9fbe6c55b5e6320a56144afa3
Dobby233Liu <papaqi1234567@126.com>
2019-02-05 13:55:50 +0800

    fix

--- assets/js/app_13528.js
+++ assets/js/app_13528.js
@@ -1,8 +1,8 @@
 function ralsei_please_switch_your_face(){
 	// rude blusher: ralsei
-	if(document.getElementById("ralseifacer").src == "./assets/img/ralsei_hope.png"){
-		document.getElementById("ralseifacer").src = "./assets/img/ralsei_blush.png"; 
+	if(document.getElementById("ralseifacer").src == "assets/img/ralsei_hope.png"){
+		document.getElementById("ralseifacer").src = "assets/img/ralsei_blush.png"; 
 	} else {
-		document.getElementById("ralseifacer").src = "./assets/img/ralsei_hope.png";
+		document.getElementById("ralseifacer").src = "assets/img/ralsei_hope.png";
 	}
 }
